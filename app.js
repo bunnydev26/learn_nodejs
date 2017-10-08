19c993ba2cd52be963236f085f99745d68e57ca1
Amit Thapa <amit@okay.com>
2017-10-08 23:25:31 +0530

    Partial View and static files

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/assets', express.static('assets'));
 
 app.listen(9876, function() {
 	console.log("Listening at port 9876");
 });
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + "/index.html");
+	res.render('index');
 });
 
 app.get('/contact', function(req, res) {
-	res.sendFile(__dirname + "/contact.html");
+	res.render('contact', {qs: req.query});
 });
 
 app.get('/profile/:name', function(req, res) {
-	var data = {age: 17, job: 'Architect'};
+	var data = {age: 17, job: 'Architect', hobbies: ["Eating Pizza", "Sketching", "Listening to Music", "Listening to Baby"]};
 	res.render('profile', {person: req.params.name, data: data});
 });
+
